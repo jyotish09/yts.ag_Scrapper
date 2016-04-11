5eef55d9bbcb074a1954f92a30625749311f2539
jyotish09 <dontsentmemails@yahoo.com>
2016-04-11 18:13:18 +0530

    RT Rating

--- YTS_AG.py
+++ YTS_AG.py
@@ -5,15 +5,26 @@ import re
 session = requests.session()
 page = session.post('https://yts.ag/')
 
-##Home Page details
+#Home Page details
+
+#Popular Movies
+
 popular_movies = BS(page.text,"html.parser").find('div',{'id':'popular-downloads'})
+##print(popular_movies)
 popular_movies_rating = popular_movies.find_all('h4',{'class':'rating'})
 popular_movies = popular_movies.find_all('a',{'class':'browse-movie-title'})
 
-print(popular_movies)
+##print(popular_movies)
+##print('\n')
+
+pMR=[]
+
+for i in popular_movies_rating:
+    pMR.append(re.search('(\d+\.\d+)',i.get_text()).group())
+
+##print (pMR)
 print('\n')
-print(popular_movies_rating)
-print('\n')
+
 
 pM=[]
 
@@ -23,5 +34,11 @@ for i in popular_movies:
 
 print ('Trending Downloads in YTS: ' + '\n')
 
-for i in pM:
-    print(i + '\n')
+for i,j in zip(pM,pMR):
+    print(i+" - (Rotten Tomatoes : "+j+")")
+
+print('\n')
+
+
+#Latest Movies
+
